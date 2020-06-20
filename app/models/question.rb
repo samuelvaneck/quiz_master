8fd5c5fc4aca6b4c762eec63e12bd934832f5142
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-06-20 16:32:30 +0200

    Add awser model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  has_many :awnsers
+
   validates :content, :position, presence: true
   validates :position, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_123328) do
+ActiveRecord::Schema.define(version: 2020_06_20_142507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "awnsers", force: :cascade do |t|
+    t.string "content"
+    t.integer "score"
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_awnsers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "content"
