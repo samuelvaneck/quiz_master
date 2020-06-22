467ba4f2ae415bd8c57745e52afc4275be476301
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-06-22 16:26:29 +0200

    Filled seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+question = Question.create({ content: 'What is your favorite versioning tool?' })
+Awnser.create({ content: 'SVN', score: 0, question: question })
+Awnser.create({ content: 'GIT', score: 10, question: question })
+Awnser.create({ content: 'CSV', score: -5, question: question })
+Awnser.create({ content: 'Mercurial', score: 5, question: question })
+Awnser.create({ content: 'Ehh, what?', score: 0, question: question })
+
+question = Quiest.create({ content: 'What is your favorite cat?' })
+Awnser.create({ content: 'Ceiling cat', score: 10, question: question })
+Awnser.create({ content: 'Invisible bike cat', score: 5, question: question })
+Awnser.create({ content: 'Octocat', score: 12, question: question })
+Awnser.create({ content: 'Monorail cat', score: 2, question: question })
+
+question = Quiest.create({ content: 'What is your favorite gemstone?' })
+Awnser.create({ content: 'Diamond', score: 5, question: question })
+Awnser.create({ content: 'Ruby', score: 10, question: question })
+Awnser.create({ content: 'Kryptonite', score: 10, question: question })
+Awnser.create({ content: 'Emerald', score: -5, question: question })
