7c7d3637b8892367a0832cf6ae6759710a9bc1a4
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-06-22 16:35:34 +0200

    Fix issues seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
-question = Question.create({ content: 'What is your favorite versioning tool?' })
+# frozen_string_literal: true
+
+question = Question.create!({ content: 'What is your favorite versioning tool?', position: 0 })
 Awnser.create({ content: 'SVN', score: 0, question: question })
 Awnser.create({ content: 'GIT', score: 10, question: question })
 Awnser.create({ content: 'CSV', score: -5, question: question })
 Awnser.create({ content: 'Mercurial', score: 5, question: question })
 Awnser.create({ content: 'Ehh, what?', score: 0, question: question })
 
-question = Quiest.create({ content: 'What is your favorite cat?' })
+question = Question.create!({ content: 'What is your favorite cat?', position: 1 })
 Awnser.create({ content: 'Ceiling cat', score: 10, question: question })
 Awnser.create({ content: 'Invisible bike cat', score: 5, question: question })
 Awnser.create({ content: 'Octocat', score: 12, question: question })
 Awnser.create({ content: 'Monorail cat', score: 2, question: question })
 
-question = Quiest.create({ content: 'What is your favorite gemstone?' })
+question = Question.create!({ content: 'What is your favorite gemstone?', position: 2 })
 Awnser.create({ content: 'Diamond', score: 5, question: question })
 Awnser.create({ content: 'Ruby', score: 10, question: question })
 Awnser.create({ content: 'Kryptonite', score: 10, question: question })
